3b2a2181b591998adea4415c23fc6f4fe5365d62
Moringa School <brunolalpachino@gmail.com>
2022-03-20 21:10:59 +0300

    tweaking of js

--- js/order.js
+++ js/order.js
@@ -14,9 +14,47 @@ function order(e) {
     }
 
 
+    var toppingEl = document.getElementById('toppings').value;
+    var toppingPrice = 0;
 
-    console.log(priceEl);
+
+    if (toppingEl === 'olive') {
+        toppingPrice = 50;
+    }
+    if (toppingEl === 'cheese') {
+        toppingPrice = 100;
+    }
+    if (toppingEl === 'peproni') {
+        toppingPrice = 90;
+    }
+
+    // crust section////////////////////////////
+
+    var crustEL = document.getElementById('crust').value;
+    var crustPrice = 0;
+
+
+    if (crustEL === 'stuffed') {
+        crustPrice = 50;
+
+    }
+    if (crustEL === 'gluten') {
+        crustPrice = 30;
+    }
+    if (crustEL === 'crispy') {
+        crustPrice = 90;
+    }
+
+
+    var total = toppingPrice + crustPrice + priceEl
 
 
 
+
+    var finalEl = document.getElementById('')
+
+
+
+
+    console.log(total);
 }
