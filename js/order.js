4dc64747f248ae9af51ff27af6725f04ea726963
Moringa School <brunolalpachino@gmail.com>
2022-03-20 18:50:31 +0300

    ft == defined function methods

--- js/order.js
+++ js/order.js
@@ -1,15 +1,21 @@
 function order(e) {
-    event.preventDefault
+    event.preventDefault();
 
     var orderEl = document.querySelector('input[name="pizza"]:checked').value;
 
 
     var priceEl = 0
 
-    if (orderEl === large) {
+    if (orderEl === 'large') {
         priceEl = 1000;
     }
-    console.log(orderEl)
+    if (orderEl === 'medium') {
+        priceEl = 650
+    }
+
+
+
+    console.log(priceEl);
 
 
 
