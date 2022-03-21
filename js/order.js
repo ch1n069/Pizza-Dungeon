18ec3cc682fca9267419045d3f8752971372d2b6
Moringa School <brunolalpachino@gmail.com>
2022-03-21 13:55:26 +0300

    ft == logic fo calculating final order

--- js/order.js
+++ js/order.js
@@ -1,7 +1,21 @@
 function order(e) {
     event.preventDefault();
 
+
+
+
+
+
+
+
+
+
+    // var deliveryEl = document.getElementById('delivery').value
+
+    // console.log(deliveryEl)
+
     var orderEl = document.querySelector('input[name="pizza"]:checked').value;
+
 
 
     var priceEl = 0
@@ -44,9 +58,29 @@ function order(e) {
     if (crustEL === 'crispy') {
         crustPrice = 90;
     }
+    // ///////hides order card
+    $('.card1').hide()
+
+    $('#an1,#checkout,#table,#proceed').show()
+
+
+
+    var deliveryEl = document.getElementById('location').value
+    var bike = 0
+
+    if (deliveryEl != 0) {
+        bike = 200
+    }
+
+    console.log(bike)
 
 
     var total = toppingPrice + crustPrice + priceEl
+
+
+    var withDelivery = toppingPrice + crustPrice + priceEl + bike
+
+
 
 
 
@@ -61,7 +95,11 @@ function order(e) {
     $('#pizzaOrder').html(orderEl + " " + "pizza");
     $('#toppingsOrder').html(toppingEl + " ");
     $('#crustOrder').html(crustEL + " ")
-    $('#totalOrder').html(total)
+    $('#deliveryOrder').html(deliveryEl + " ")
+    $('#totalOrder').html(total + " ")
+
+
+
 
 
 
@@ -78,23 +116,32 @@ function order(e) {
 
     console.log(total);
 
+
+
+
+
+
+
+
     $(document).ready(function() {
 
-        $('#checkout').on('click', function() {
+        $('#proceed').on('click', function() {
             Swal.fire({
-                title: 'Do you want to add delivery fee',
+                title: 'You are about to checkout',
                 showDenyButton: true,
                 showCancelButton: true,
-                confirmButtonText: 'Yes',
-                denyButtonText: `No`,
+                confirmButtonText: 'No delivery',
+                denyButtonText: `with delivery`,
             }).then((result) => {
                 /* Read more about isConfirmed, isDenied below */
                 if (result.isConfirmed) {
-                    swal.fire('<input></input')
+
+
                     Swal.fire('Order has been recieved', 'your total is ' + total, 'success');
                 } else if (result.isDenied) {
-                    Swal.fire('You can pick up your order at the shop ', 'your total will be ' + total, 'success')
+                    Swal.fire('order has been recieved ', 'your total is ' + withDelivery, 'success');
                 }
+
             })
 
 
